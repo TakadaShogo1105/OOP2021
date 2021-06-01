3df90e47b04f371ce01cb091e669e7ba44a8d09c
高田　将吾 <32218@ojs.ac.jp>
2021-06-01 10:26:04 +0900

    P79 問題2,2　完成

---
+++ Exercise2/InchConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+
+    public static class InchConverter {
+
+        private const double ratio = 0.0254;
+
+        static public double FromMeter(double meter) {
+            return meter * ratio;
+        }
+
+        static public double ToMeter(double inch) {
+            return inch / ratio;
+        }
+    }
+}
---
+++ Exercise2/Properties/FeetConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+
+    public static class FeetConverter {
+
+        private const double ratio = 0.3048;
+
+        static public double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        static public double ToMeter(double feet) {
+            return feet * ratio;
+        }
+    }
+}
