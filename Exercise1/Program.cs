c0fbcd4c55e25a2838ed4f0da654b148dcb480c7
高田　将吾 <32218@ojs.ac.jp>
2021-06-01 09:31:48 +0900

    P79　問題2,1完成

---
+++ Exercise1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+
+        static void Main(string[] args) {
+
+            var songs = new Song[] {
+                new Song("アグレッシ部","KREVA",5),
+            };
+            PrintSongs(songs);
+        }
+
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine(@"{0},{1},{2:m\:ss}",song.Title,song.ArtistName,TimeSpan.FromSeconds(song.Length));
+                //Console.WriteLine(song.Title);
+                //Console.WriteLine(song.ArtistName);
+                //Console.WriteLine(song.Length);
+            }
+        }
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    //2.1.1
+    class Song {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+      
+
+        //2.1.2
+        public Song(string title, string artistName, int length) {
+
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+
+        }
+    }
+}
