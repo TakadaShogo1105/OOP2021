06971dd313f2e404970093fb09b17cb7e9d67d6e
高田　将吾 <32218@ojs.ac.jp>
2021-06-21 13:35:42 +0900

    一期目試験

---
+++ Chapter05/Exercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+            //#region 問題5.1
+            //Console.WriteLine("文字を入力");
+            //var s1 = Console.ReadLine();
+
+            //Console.WriteLine("文字を入力");
+            //var s2 = Console.ReadLine();
+
+            //if (String.Compare(s1, s2, ignoreCase: true) == 0) {
+            //    Console.WriteLine("等しい");
+            //} else {
+            //    Console.WriteLine("等しくない");
+            //}
+            //#endregion 
+
+            #region　問題5.2
+            Console.WriteLine("文字を入力");
+            int num1 = int.Parse(Console.ReadLine());
+            var n1 = num1.ToString("#,0");
+            Console.WriteLine(n1);
+            #endregion 
+        }
+    }
+}
