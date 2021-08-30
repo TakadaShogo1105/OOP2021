6d8a419debde7636e46d5ede85abe74163cd1365
将吾 髙田 <32218@ojs.ac.jp>
2021-08-30 15:16:37 +0900

    11.1.4終わり

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -19,7 +19,12 @@ namespace Exercise1 {
             Exercise1_3(file);
             Console.WriteLine("-------");
 
+            Exercise1_4(file);
+            Console.WriteLine("-------");
+
         }
+
+        
 
         private static void Exercise1_1(string file) {
             var xdoc = XDocument.Load(file);
@@ -57,5 +62,18 @@ namespace Exercise1 {
 
             Console.WriteLine("{0}",sports.Name);
         }
+
+        private static void Exercise1_4(string file) {
+            var xdoc = XDocument.Load(file);
+            var element = new XElement("file",
+                            new XElement("name", "サッカー", new XAttribute("kanji", "蹴玉")),
+                            new XElement("firstplayed", "1872-11-30"),
+                            new XElement("teammember", "11"));
+
+            xdoc.Root.Add(element);
+
+            xdoc.Save("Sports.xml");//XML　ファイルに保存
+        }
+
     }
 }
--- Chapter11/Section02/Program.cs
+++ Chapter11/Section02/Program.cs
@@ -9,24 +9,26 @@ namespace Section02 {
     class Program {
         static void Main(string[] args) {
 
-            string elmstring =
-            @"<novelist>
-                  <name kana=""きくち かん"">菊池 寛</name>
-                  <birth>1888-12-26</birth>
-                  <death>1948-03-06</death>
-                  <masterpieces>
-                    <title>恩讐の彼方に</title>
-                    <title>真珠夫人</title>
-                  </masterpieces>
-                </novelist>";
+            //string elmstring =
+            //@"<novelist>
+            //      <name kana=""きくち かん"">菊池 寛</name>
+            //      <birth>1888-12-26</birth>
+            //      <death>1948-03-06</death>
+            //      <masterpieces>
+            //        <title>恩讐の彼方に</title>
+            //        <title>真珠夫人</title>
+            //      </masterpieces>
+            //    </novelist>";
 
-            XElement element = XElement.Parse(elmstring);
+            //XElement element = XElement.Parse(elmstring);
 
             var xdoc = XDocument.Load("novelists.xml");
-            xdoc.Root.Add(element);
+            //xdoc.Root.Add(element);
 
-            xdoc.Save("novelists.xml");//XML　ファイルに保存
-
+            //xdoc.Save("novelists.xml");//XML　ファイルに保存
+            var elements = xdoc.Elements().Where(x => x.Element("name").Value == "菊池 寛");
+            elements.Remove();
+            xdoc.Save("novelists.xml");
         }
 
     }
