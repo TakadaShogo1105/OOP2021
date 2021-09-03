0318b12f88d4e0e1867069edd4dd1709b33e46f4
将吾 髙田 <32218@ojs.ac.jp>
2021-09-03 14:49:20 +0900

    0903演習問題1おわり

--- Chapter14/ConsoleApp1/Program.cs
+++ Chapter14/ConsoleApp1/Program.cs
@@ -8,28 +8,78 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
+
 namespace Section04 {
     class Program {
         static void Main(string[] args) {
             new Program();
-        } 
-
-        public Program() {
-            //DownloadFileAsync();
-            //OpenReadSample();
-            var results = GetWeatherReportFromYahoo(4610);
-            foreach (var s in results) {
-                Console.WriteLine(s);
-            }
-            Console.ReadLine();
         }
 
+
+
+        //コンストラクタ
+        public Program() {
+
+            Console.WriteLine("地域コード入力");
+            Console.WriteLine("1:前橋\n2:みなかみ\n3:宇都宮\n4:水戸\n9:その他（直接入力)");
+            Console.Write(">");
+            int num = int.Parse(Console.ReadLine());
+
+
+
+            switch (num) {
+                case 1:
+                    num = 4210;
+                    break;
+                case 2:
+                    num = 4220;
+                    break;
+                case 3:
+                    num = 4110;
+                    break;
+                case 4:
+                    num = 4010;
+                    break;
+                case 9:
+                    Console.WriteLine("コードを入力して下さい");
+                    Console.Write(">");
+                    num = int.Parse(Console.ReadLine());
+                    break;
+                default:
+                    Console.WriteLine("正しく入力して下さい");
+                    new Program();
+                    break;
+            }
+
+
+
+
+            var results = GetWeatherReportFromYahoo(num);
+            foreach (var s in results) {
+                Console.WriteLine(s);
+
+
+
+            }
+
+
+
+        }
+
+
+
+        //リスト14.15
         public void DownloadString() {
             var wc = new WebClient();
             wc.Encoding = Encoding.UTF8;
             var html = wc.DownloadString("https://www.yahoo.co.jp/");
             Console.WriteLine(html);
         }
+
+
+
+        //リスト14.17
         private void DownloadFileAsync() {
             var wc = new WebClient();
             var url = new Uri(@"C:\Users\st32218\Desktop\DB & MOSなど.zip");
@@ -37,8 +87,10 @@ namespace Section04 {
             wc.DownloadProgressChanged += wc_DownloadProgressChanged;
             wc.DownloadFileCompleted += wc_DownloadFileCompleted;
             wc.DownloadFileAsync(url, filename);
-            Console.ReadLine();
+            Console.ReadLine();//アプリケーションが終了しないようにする
         }
+
+
 
         static void wc_DownloadProgressChanged(object sender,
                             DownloadProgressChangedEventArgs e) {
@@ -46,23 +98,31 @@ namespace Section04 {
                               e.BytesReceived, e.TotalBytesToReceive);
         }
 
+
+
         static void wc_DownloadFileCompleted(object sender,
                             System.ComponentModel.AsyncCompletedEventArgs e) {
             Console.WriteLine("ダウンロード完了");
         }
 
-        //リスト14.18(ストリームとしてダウンロード)
+
+
+        //リスト14.18
         public void OpenReadSample() {
             var wc = new WebClient();
-            using (var stream = wc.OpenRead(@"https://yahoo.co.jp/")) 
-            using (var sr = new StreamReader(stream,Encoding.UTF8)) {
-
+            using (var stream = wc.OpenRead("https://yahoo.co.jp/"))
+            using (var sr = new StreamReader(stream, Encoding.UTF8)) {
                 string html = sr.ReadToEnd();
                 Console.WriteLine(html);
             }
 
+
+
         }
 
+
+
+        //リスト14.19
         private static IEnumerable<string> GetWeatherReportFromYahoo(int cityCode) {
             using (var wc = new WebClient()) {
                 wc.Headers.Add("Content-type", "charset=UTF-8");
@@ -70,6 +130,8 @@ namespace Section04 {
                     @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", cityCode);
                 var url = new Uri(uriString);
                 var stream = wc.OpenRead(url);
+
+
 
                 XDocument xdoc = XDocument.Load(stream);
                 var nodes = xdoc.Root.Descendants("title");
@@ -79,5 +141,8 @@ namespace Section04 {
                 }
             }
         }
+
+
+
     }
 }
---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "前橋"});
+            this.comboBox1.Location = new System.Drawing.Point(124, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "天気を表示";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 104);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(450, 283);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "地域を選択";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
